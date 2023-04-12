41f6d651630e2b6c5c5efb7b25bf7ed71352fc84
Ashikanair119 <130470605+Ashikanair119@users.noreply.github.com>
2023-04-12 18:19:11 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,41 @@
+# x="shigin"
+# i=1
+# while i<=10:
+#     print(i)
+#     i=i+1
+#     if i==4:
+#        break
+#x='ashika'
+#i=1
+#for i in range(1,6,2):
+#    print(x)
+#i=1
+#for i in range(5,100,5):
+ #   print(i)
+#x=['rfg','grg']
+#for i in x:
+#     print(i)
+
+#x='ashika'
+#for i in x:
+#    print(i)
+
+#adj=['red','big','tasty']
+#fruits=['vyygv','g7yg77','ddtrr']
+#for x in adj:
+#    for y in fruits:
+#        print(x,y)
+
+
+
+a=[]
+b=[]
+for i in range(1,101):
+    a.append(i)
+print(a)
+for i in a:
+    if i%2==0:
+        b.append(i)
+print(b)        
+
+    
